85f486a5011333333772a3d9bf6f54daaad092c1
mantosh7 <mahiraval000@gmail.com>
2025-01-04 03:07:21 +0530

    change

--- Bit Manipulation/PowerOf2orNot.c
+++ Bit Manipulation/PowerOf2orNot.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
+
+void power_or_not(int num)
+{
+    int count = 0;
+    while(num!=0)
+    {
+        if((num&1)==1) count++;
+        num = num>>1;
+    }
+    if(count==1) printf("true");
+    else printf("false");
+    return;
+}
+
 int main()
 {
-    int num = 3;
-    if((num&1)==1) printf("false");
-    else printf("true");
+    int num = 8;
+    power_or_not(num);
     return 0;
 }
