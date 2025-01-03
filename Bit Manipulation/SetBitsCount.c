a50ff9a1946ca2c4cd41d9b305a961abd7b6d7ed
mantosh7 <mahiraval000@gmail.com>
2025-01-04 03:09:14 +0530

    change

--- Bit Manipulation/SetBitsCount.c
+++ Bit Manipulation/SetBitsCount.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
-int main()
+
+int count_set_bits(int num)
 {
-    int num = 2147483645;
     int count = 0;
     while(num!=0)
     {
         if((num&1)==1) count++;
         num = num>>1;
     }
+    return count;
+}
+int main()
+{
+    int num = 2147483645;
+    int count = count_set_bits(num);
     printf("No.of set bits = %d",count);
     return 0;
 }
