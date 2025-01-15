1fcb89a8bb9172ee9345e8cca4533f293a9930de
mantosh7 <mahiraval000@gmail.com>
2025-01-15 18:37:42 +0530

    replacing traingle with pyramid

--- Pattern Questions/ButterFly.c
+++ Pattern Questions/ButterFly.c
@@ -23,17 +23,17 @@ int main()
 
     for(int i=0;i<n-1;i++)
     {
-        // 1st traiangle
+        // 1st pyramid
         for(int j=0;j<=i;j++)
         {
             printf("*");
         }
-        // space
+        // space traingle
         for(int j=2*(n-1)-1-2*i;j>0;j--)
         {
             printf(" ");
         }
-        // 2nd traingle
+        // 2nd pyramid
         for(int j=0;j<=i;j++)
         {
             printf("*");
@@ -50,17 +50,17 @@ int main()
     //  Lower part
     for(int i=0;i<n-1;i++)
     {
-        // 1st traingle
+        // 1st pyramid
         for(int j=n-1-i;j>0;j--)
         {
             printf("*");
         }
-        // spaces
+        // spaces triangle
         for(int j=0;j<2*i+1;j++)
         {
             printf(" ");
         }
-        // 2nd traingle
+        // 2nd pyramid
         for(int j=n-1-i;j>0;j--)
         {
             printf("*");
