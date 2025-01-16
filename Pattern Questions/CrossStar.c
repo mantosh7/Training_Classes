7d7e5a593f8475cb54daec522c29a2f8e6db118c
mantosh7 <mahiraval000@gmail.com>
2025-01-16 20:33:55 +0530

    Cross star pattern

---
+++ Pattern Questions/CrossStar.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter no.of rows:");
+    scanf("%d",&n);
+
+    // Upper Part
+    for(int i=0;i<n;i++)
+    {
+        // starting spaces
+        for(int j=0;j<2*i;j++)
+        {
+            printf(" ");
+        }
+        // stars
+        for(int j=0;j<n-i;j++)
+        {
+            printf("*");
+        }
+        // Inverted pyramid of spaces
+        for(int j=0;j<(2*n-2)-2*i;j++)
+        {
+            printf(" ");
+        }
+        // stars
+        for(int j=0;j<n-i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");        
+    }
+
+    // Lower Part
+    for(int i=0;i<n-1;i++)
+    {
+        // spaces
+        for(int j=0;j<(2*n-4)-2*i;j++)
+        {
+            printf(" ");
+        }
+        // stars
+        for(int j=0;j<i+2;j++)
+        {
+            printf("*");
+        }
+        // pyramid of spaces
+        for(int j=0;j<2*i+2;j++)
+        {
+            printf(" ");
+        }
+        // stars
+        for(int j=0;j<i+2;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
